f325cf23f8d58a0594b5ee5467e78a61bc64191e
Hugo <gitjohan5@gmail.com>
2024-05-18 21:41:39 +0300

    fixed prisma

--- auth.ts
+++ auth.ts
@@ -4,7 +4,6 @@ import { db } from "./utils/db";
 import google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  theme: {},
   adapter: PrismaAdapter(db),
   providers: [google],
 });
