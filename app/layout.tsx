d8cb873d9e434b4e42432f96df491415eb7252c8
Hugo <gitjohan5@gmail.com>
2024-05-25 13:46:16 +0300

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main className="max-w-[1200px] mx-auto">
           <SessionProvider>{children}</SessionProvider>
           <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
